73faf9002fe4b96cd7d5bd2fcf93965137299c94
Mohammed Eshan Jinabade <eshajina280403@gmail.com>
2026-01-05 21:21:36 +0530

    Fix: log Google Client ID for OAuth debugging agin

--- src/main.jsx
+++ src/main.jsx
@@ -6,28 +6,31 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./index.css";
 
-// Vite env variable (use import.meta.env)
+// Read Google Client ID from Vite env
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+// Debug log (keep this until OAuth works)
 console.log("Google Client ID loaded:", clientId);
 
-
+// Optional warning if env is missing
 if (!clientId) {
-  // eslint-disable-next-line no-console
   console.warn(
-    "VITE_GOOGLE_CLIENT_ID is NOT set. Google OAuth will fail with missing client_id. " +
-      "Make sure .env contains VITE_GOOGLE_CLIENT_ID and restart the dev server."
+    "VITE_GOOGLE_CLIENT_ID is NOT set. Google OAuth will not work."
   );
-} else {
-  // eslint-disable-next-line no-console
-  console.log("Google Client ID loaded:", clientId);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
+    {clientId ? (
+      <GoogleOAuthProvider clientId={clientId}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    ) : (
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </GoogleOAuthProvider>
+    )}
   </React.StrictMode>
 );
